c8a36408cc7d80600a17c92c5f85659c3c3b81ce
SangaviRajamanickam <160992565+SangaviRajamanickam@users.noreply.github.com>
2024-02-26 21:03:19 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package week2Day1;
+
+public class Calculator {
+
+	public void addition() {
+		int a=20;
+		int b=30;
+		int c=a+b;
+		
+		System.out.println(c);
+	
+	}
+	
+
+	
+	}
+	
+	
+	
+	
+	
+		
+		
+	}
+
+
