5a8a7df25c3313fd6e57bc7a1798d0df34a682fe
SangaviRajamanickam <160992565+SangaviRajamanickam@users.noreply.github.com>
2024-02-26 20:59:52 +0530

    swapping number

---
+++ SwappngTwoNumber_UseTemVariable.java
@@ -0,0 +1,26 @@
+package week2Day1;
+
+public class SwappngTwoNumber_UseTemVariable {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		int b=80;
+		
+		
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("The vakue of A:" +a);
+		System.out.println("The value of B:" +b);
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ SwappngTwoNumber_WithoutTepVariable.java
@@ -0,0 +1,23 @@
+package week2Day1;
+
+public class SwappngTwoNumber_WithoutTepVariable {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		int b=80;
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println("The value of A:" +a);
+		System.out.println("The value of B:" +b);
+		
+		
+		
+		
+		
+	}
+
+}
